0b921c000e30ae970cf30ffe4da45edb270e6919
phuang00 <phuang00@stuy.edu>
2019-03-05 12:24:30 -0500

    updated constructor to read in elevations into a 2D array

--- USACO.java
+++ USACO.java
@@ -3,12 +3,30 @@ import java.util.*;
 
 public class USACO{
 
-  public static int bronze(String filename){
-    
+  public static int bronze(String filename) throws FileNotFoundException{
+    File f = new File(filename);
+    Scanner in = new Scanner(f);
+    String[] vars = in.nextLine().split(" ");
+    int[][] pasture = new int[Integer.parseInt(vars[0])][Integer.parseInt(vars[1])];
+    for (int i = 0; i < pasture.length; i++){
+      String[] tempRow = in.nextLine().split(" ");
+      for (int j = 0; j < pasture[0].length; j++){
+        pasture[i][j] = Integer.parseInt(tempRow[j]);
+      }
+      System.out.println(Arrays.toString(pasture[i]));
+    }
+    return -1;
   }
 
   public static int silver(String filename){
-
+    return -1;
   }
 
+  public static void main(String[] args) {
+    try{
+      bronze("makelake.1.in");
+    } catch (FileNotFoundException e){
+
+    }
+  }
 }
