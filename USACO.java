330bae316f88fe3b61f1b8f61682d4b76dbdcd7f
phuang00 <phuang00@stuy.edu>
2019-03-05 16:28:54 -0500

    finished bronze problem

--- USACO.java
+++ USACO.java
@@ -13,9 +13,53 @@ public class USACO{
       for (int j = 0; j < pasture[0].length; j++){
         pasture[i][j] = Integer.parseInt(tempRow[j]);
       }
-      System.out.println(Arrays.toString(pasture[i]));
+      //System.out.println(Arrays.toString(pasture[i]));
     }
-    return -1;
+    while(in.hasNextLine()){
+      String[] tempArgs = in.nextLine().split(" ");
+      //System.out.println(tempArgs[0]);
+      //System.out.println(tempArgs[1]);
+      //System.out.println(tempArgs[2]);
+      updateLand(pasture, Integer.parseInt(tempArgs[0]) - 1, Integer.parseInt(tempArgs[1]) - 1, Integer.parseInt(tempArgs[2]));
+      /*for (int i = 0; i < pasture.length; i++){
+        System.out.println(Arrays.toString(pasture[i]));
+      }*/
+    }
+    return addElevation(pasture, Integer.parseInt(vars[2])) * 72 * 72;
+  }
+
+  private static void updateLand(int[][] land, int row, int col, int deep){
+    ArrayList<Integer> eles = new ArrayList<>();
+    for (int i = 0; i < 3; i++){
+      for (int j = 0; j < 3; j++){
+        eles.add(land[row + i][col + j]);
+      }
+    }
+    int target = Collections.max(eles) - deep;
+    //System.out.println(target);
+    for (int i = 0; i < 3; i++){
+      for (int j = 0; j < 3; j++){
+        if (land[row + i][col + j] > target) land[row + i][col + j] = target;
+      }
+    }
+  }
+
+  public static int addElevation(int[][] land, int deep){
+    int ans = 0;
+    for (int i = 0; i < land.length; i++){
+      for (int j = 0; j < land[i].length; j++){
+        if (land[i][j] > deep) land[i][j] = 0;
+        else {
+          land[i][j] = deep - land[i][j];
+          ans += land[i][j];
+        }
+      }
+    }
+    /*for (int i = 0; i < land.length; i++){
+      System.out.println(Arrays.toString(land[i]));
+    }
+    System.out.println(ans);*/
+    return ans;
   }
 
   public static int silver(String filename){
@@ -24,7 +68,7 @@ public class USACO{
 
   public static void main(String[] args) {
     try{
-      bronze("makelake.1.in");
+      System.out.println(bronze("makelake.1.in"));
     } catch (FileNotFoundException e){
 
     }
